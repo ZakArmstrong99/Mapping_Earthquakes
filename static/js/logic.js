037a3bc0042fd7b1638c3b926491d894c01bccf7
Zak Armstrong <armstrong.816@osu.edu>
2022-09-01 15:09:29 -0400

    added json data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,9 @@
-// Add console.log to check to see if our code is working.
 console.log("working");
 
 
 
-
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -14,20 +12,41 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [37.62, -122.370],
-  [30.1900, -97.6687],
-  [40.6413, -73.7781],
-  [43.678524, -79.6291]
-];
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "blue",
-  weight: 4,
-  dashArray: '5, 10'
-}).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h3>" + "Airport code:" + feature.properties.faa + "</h3>" + "<hr>" + "<h4>" + "Airport name:" + feature.properties.name + "</h4>");
+  }
+
+}).addTo(map);
+
+
+
