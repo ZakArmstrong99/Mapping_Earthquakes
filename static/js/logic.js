96d336d6fd9bd5099e03722962a38171c7f48beb
Zak Armstrong <armstrong.816@osu.edu>
2022-09-05 14:00:15 -0400

    initial

--- static/js/logic.js
+++ static/js/logic.js
@@ -34,15 +34,11 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/ZakArmstrong99/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
 });
-
 
 
 
